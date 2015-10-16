c753d97ec95bdf23673c1c623fe53103f482e58d
Michael Schilling <mschilling@move4mobile.com>
2015-10-16 22:28:02 +0200

    Update toast message

--- app/src/main/java/nl/arkid/myappportfolio/MainActivity.java
+++ app/src/main/java/nl/arkid/myappportfolio/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) view;
 
         Context context = getApplicationContext();
-        CharSequence text = "Hello " + button.getText();
+        CharSequence text = "This will launch " + button.getText();
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
